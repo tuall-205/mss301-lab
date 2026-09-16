d9d785a8f4b9be816774841799e7b03f2c0628b1
Nguyen Huu Minh Tuan <nguyenhuuminhtuan20111@gmail.com>
2026-09-16 20:58:59 +0700

    feat(controller): add ProductController with create and getAll endpoints

---
+++ Slot 3/product_service/src/main/java/com/fudn/productservice/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.fudn.productservice.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fudn.productservice.dto.ProductRequest;
+import com.fudn.productservice.dto.ProductResponse;
+import com.fudn.productservice.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
