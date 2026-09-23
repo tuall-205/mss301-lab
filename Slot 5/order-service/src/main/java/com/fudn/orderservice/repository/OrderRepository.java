42b4093b1b824f3d0537b83ebcbbd4adb0567050
Nguyen Huu Minh Tuan <nguyenhuuminhtuan20111@gmail.com>
2026-09-23 19:29:17 +0700

    feat(repository): add order repository

--- Slot 5/order-service/src/main/java/com/fudn/orderservice/repository/OrderRepository.java
+++ Slot 5/order-service/src/main/java/com/fudn/orderservice/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.fudn.orderservice.repository;
 
-import com.fudn.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fudn.orderservice.model.Order;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    
 }
