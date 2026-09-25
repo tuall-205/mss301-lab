54cf66246862d31ac889c91a4bee8857c6a0171f
Nguyen Huu Minh Tuan <nguyenhuuminhtuan20111@gmail.com>
2026-09-25 18:44:37 +0700

    test(order): add InventoryStubs for mocking inventory-service with WireMock

---
+++ Slot 6/ShoppingServices/order-service/src/test/java/com/fudn/orderservice/stub/InventoryStubs.java
@@ -0,0 +1,21 @@
+package com.fudn.orderservice.stub;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class InventoryStubs {
+
+    public void stubInventoryCall(String skuCode, Integer quantity) {
+        stubFor(get(urlEqualTo(
+                "/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("true")));
+    }
+}
